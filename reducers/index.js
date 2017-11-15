82af32a72a576ba2e5670f2da0dcb59cae8f562c
Josh Weaver <jweaver911@gmail.com>
2017-11-14 22:02:29 -0600

    remove: console logs

--- reducers/index.js
+++ reducers/index.js
@@ -1,54 +1,50 @@
 import { RECEIVE_DECKS, ADD_DECK, REMOVE_DECK, ADD_CARD } from "../actions";
 
-const initialState = {
-  React: {
-    title: "React",
-    questions: [
-      {
-        question: "What is React?",
-        answer: "A library for managing user interfaces"
-      },
-      {
-        question: "Where do you make Ajax requests in React?",
-        answer: "The componentDidMount lifecycle event"
-      }
-    ]
-  },
-  JavaScript: {
-    title: "JavaScript",
-    questions: [
-      {
-        question: "What is a closure?",
-        answer:
-          "The combination of a function and the lexical environment within which that function was declared."
-      }
-    ]
-  }
-};
+// const initialState = {
+//   React: {
+//     title: "React",
+//     questions: [
+//       {
+//         question: "What is React?",
+//         answer: "A library for managing user interfaces"
+//       },
+//       {
+//         question: "Where do you make Ajax requests in React?",
+//         answer: "The componentDidMount lifecycle event"
+//       }
+//     ]
+//   },
+//   JavaScript: {
+//     title: "JavaScript",
+//     questions: [
+//       {
+//         question: "What is a closure?",
+//         answer:
+//           "The combination of a function and the lexical environment within which that function was declared."
+//       }
+//     ]
+//   }
+// };
 
 function decks(state = {}, action) {
   switch (action.type) {
     case RECEIVE_DECKS:
-      console.log("[REDUCER] RECEIVE_DECKS", state, action);
       return {
         ...state,
         ...action.decks
       };
     case ADD_DECK:
-      console.log("[REDUCER] ADD_DECK", state, action);
       return {
         ...state,
         [action.deck.title]: { ...action.deck }
       };
     case REMOVE_DECK:
-      console.log("[REDUCER] REMOVE_DECK", state, action);
       const decks = { ...state };
       delete decks[action.title];
       return {
         ...decks
       };
     case ADD_CARD:
-      console.log("[REDUCER] ADD_CARD", state, action);
       const deck = state[action.title];
       deck.questions.push(action.card);
       return {
--- utils/api.js
+++ utils/api.js
@@ -3,22 +3,19 @@ import { AsyncStorage } from "react-native";
 export const STORAGE_KEY = "mobileflashcards:decks";
 
 // For Development only
-export function clearAllDecks(callback) {
-  return AsyncStorage.multiRemove([STORAGE_KEY], err => console.log(err));
-}
+// export function clearAllDecks(callback) {
+//   return AsyncStorage.multiRemove([STORAGE_KEY], err => console.log(err));
+// }
 
 export function getDecks() {
   return AsyncStorage.getItem(STORAGE_KEY).then(decks => JSON.parse(decks));
 }
 
-// TODO: remove, probably don't need with redux
 export function getDeck(id) {
-  console.log("Getting single deck...", id);
   return AsyncStorage.getItem(STORAGE_KEY).then(decks => JSON.parse(decks)[id]);
 }
 
 export function saveDeckTitle(title) {
-  console.log("Saving Deck title...", title);
   return AsyncStorage.mergeItem(
     STORAGE_KEY,
     JSON.stringify({
@@ -31,7 +28,6 @@ export function saveDeckTitle(title) {
 }
 
 export function addCardToDeck(title, card) {
-  console.log("Adding card to Deck...", title, card);
   return AsyncStorage.getItem(STORAGE_KEY).then(results => {
     const decks = JSON.parse(results);
     decks[title] && decks[title].questions.push(card);
@@ -40,13 +36,11 @@ export function addCardToDeck(title, card) {
 }
 
 export function removeDeck(title, callback) {
-  console.log("REMOVING DECK", title);
   return AsyncStorage.getItem(STORAGE_KEY).then(results => {
     const decks = JSON.parse(results);
     decks[title] = undefined;
     delete decks[title];
 
-    console.log("...after delete", decks);
     AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(decks)).then(() => {
       if (typeof callback === "function") {
         callback();
