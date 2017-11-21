c206bf2294ef3acd1e273acfdde744c33c003253
Josh Weaver <jweaver911@gmail.com>
2017-11-20 19:49:14 -0600

    chore: remove commented blocks

--- reducers/index.js
+++ reducers/index.js
@@ -1,31 +1,5 @@
 import { RECEIVE_DECKS, ADD_DECK, REMOVE_DECK, ADD_CARD } from "../actions";
 
-// const initialState = {
-//   React: {
-//     title: "React",
-//     questions: [
-//       {
-//         question: "What is React?",
-//         answer: "A library for managing user interfaces"
-//       },
-//       {
-//         question: "Where do you make Ajax requests in React?",
-//         answer: "The componentDidMount lifecycle event"
-//       }
-//     ]
-//   },
-//   JavaScript: {
-//     title: "JavaScript",
-//     questions: [
-//       {
-//         question: "What is a closure?",
-//         answer:
-//           "The combination of a function and the lexical environment within which that function was declared."
-//       }
-//     ]
-//   }
-// };
-
 function decks(state = {}, action) {
   switch (action.type) {
     case RECEIVE_DECKS:
